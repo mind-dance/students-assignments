c5dc1d4e7392f8a9aa8cf8ef9da3521c71980701
Vincent <suppernoname@163.com>
2024-06-03 18:44:11 +0800

    数据库内容新建文件夹

--- client/dao.py
+++ client/dao.py
@@ -1,6 +1,17 @@
 import sqlite3
 
 
-def load_s_name():
-    pass
+class database():
+    def __init__(self):
+        # 连接数据库
+        con = sqlite3.connect('shows.db')
 
+
+    def load_students_data():
+        pass
+
+    def import_students_data():
+        pass
+    
+    def insert_student():
+        pass
---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "students" (
+	"student_id" VARCHAR NOT NULL,
+	"name" VARCHAR NULL,
+	PRIMARY KEY ("student_id"),
+	-- INDEX "ix_people_name" ("name")
+)
+
